1ad75c51903ed25dc768e029976b2c9d7a277dc9
Tobias Krischer <tobias.krischer@elyxon.de>
2020-05-11 03:42:23 +0200

    refactor to adhere to packaging standards

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rfdocgen",
-    version="0.0.1",
+    version="0.0.3",
     author="Tobias Krischer",
     author_email="tobias.krischer@elyxon.de",
     description="Generate and view Robot Framework Documentation live",
@@ -13,10 +13,22 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/tobikris/rfdocgen",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'Click==7.0',
+        'Flask==1.1.1',
+        'itsdangerous==1.1.0',
+        'Jinja2==2.10.1',
+        'livereload==2.6.1',
+        'MarkupSafe==1.1.1',
+        'six==1.12.0',
+        'tornado==6.0.3',
+        'Werkzeug==0.15.4',
+    ],
 )
